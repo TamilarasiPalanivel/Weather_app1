49f23fd19775d40d9a7c64933a49dfe1d5e7d469
TamilarasiPalanivel <168735036+TamilarasiPalanivel@users.noreply.github.com>
2024-08-07 22:21:32 +0530

    changed
    styles-changed

--- WEATHER APP/script.js
+++ WEATHER APP/script.js
@@ -3,7 +3,7 @@ function getWeather() {
     const city = document.getElementById('city').value;
 
     if (!city) {
-        alert('Please enter a city');
+        alert('enter a city');
         return;
     }
 
